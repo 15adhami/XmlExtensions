8986e6958cfff38f898f7a71a5eded154be1eccc
15adhami <15adhami@gmail.com>
2021-08-15 21:19:44 -0700

    Added XmlExtensions.Setting.ScrollView

--- Source/Setting.cs
+++ Source/Setting.cs
@@ -340,4 +340,43 @@ namespace XmlExtensions.Setting
 
         public override int getHeight() { return (bool.Parse(XmlMod.allSettings.dataDict[modId + "." + key]) ? calcHeight(caseTrue) : calcHeight(caseFalse)); }
     }
+
+    public class ScrollView : SettingContainer
+    {
+        public float height = 72;
+        public List<SettingContainer> settings;
+
+        private Vector2 scrollPos = Vector2.zero;
+
+        public override void drawSetting(Listing_Standard listingStandard, string selectedMod)
+        {
+            
+            Rect baseRect = listingStandard.GetRect(height);
+            Listing_Standard listing = new Listing_Standard();
+            Rect scrollRect = new Rect(0, 0, baseRect.width - 20f, calcHeight(settings));
+            listing.BeginScrollView(baseRect, ref scrollPos, ref scrollRect);
+            listing.verticalSpacing = listingStandard.verticalSpacing;
+            foreach (SettingContainer setting in settings)
+            {
+                setting.drawSetting(listing, selectedMod);
+            }
+            listing.EndScrollView(ref scrollRect);
+           
+        }
+
+        private int calcHeight(List<SettingContainer> settings)
+        {
+            int h = 0;
+            if (settings != null)
+            {
+                foreach (SettingContainer setting in settings)
+                {
+                    h += setting.getHeight();
+                }
+            }
+            return h;
+        }
+
+        public override int getHeight() { return ((int)height); }
+    }
 }
