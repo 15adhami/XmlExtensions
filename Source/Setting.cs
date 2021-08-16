fe093c330bdd2167bd59f122b83bf5a6b980d174
15adhami <15adhami@gmail.com>
2021-08-15 21:33:23 -0700

    Added XmlExtensions.Setting.ToggleableSettings and XmlExtensions.Setting.ScrollView

--- Source/Setting.cs
+++ Source/Setting.cs
@@ -292,4 +292,92 @@ namespace XmlExtensions.Setting
 
         public override int getHeight() { return (buttons.Count * ((spacing < 0 ? XmlMod.settingsPerMod[XmlMod.selectedMod].defaultSpacing : spacing) + 22)); }
     }
+
+    public class ToggleableSettings : SettingContainer
+    {
+        public string key;
+        public string modId;
+        public List<SettingContainer> caseTrue;
+        public List<SettingContainer> caseFalse;
+
+        public override void drawSetting(Listing_Standard listingStandard, string selectedMod)
+        {
+            List<SettingContainer> settings;
+            if (bool.Parse(XmlMod.allSettings.dataDict[modId + "." + key]))
+            {
+                settings = caseTrue;
+            }
+            else
+            {
+                settings = caseFalse;
+            }
+            if (settings != null)
+            {
+                Rect baseRect = listingStandard.GetRect(calcHeight(settings));
+                Listing_Standard listing = new Listing_Standard();
+                listing.Begin(baseRect);
+                listing.verticalSpacing = listingStandard.verticalSpacing;
+                foreach (SettingContainer setting in settings)
+                {
+                    setting.drawSetting(listing, selectedMod);
+                }
+                listing.End();
+            }
+        }
+
+        private int calcHeight(List<SettingContainer> settings)
+        {
+            int h = 0;
+            if (settings != null)
+            {
+                foreach (SettingContainer setting in settings)
+                {
+                    h += setting.getHeight();
+                }
+            }
+            return h;
+        }
+
+        public override int getHeight() { return (bool.Parse(XmlMod.allSettings.dataDict[modId + "." + key]) ? calcHeight(caseTrue) : calcHeight(caseFalse)); }
+    }
+
+    public class ScrollView : SettingContainer
+    {
+        public float height = 72;
+        public List<SettingContainer> settings;
+
+        private Vector2 scrollPos = Vector2.zero;
+
+        public override void drawSetting(Listing_Standard listingStandard, string selectedMod)
+        {
+            Rect baseRect = listingStandard.GetRect(height);            
+            Rect scrollRect = new Rect(0, 0, baseRect.width - 16f, calcHeight(settings));
+            Widgets.BeginScrollView(baseRect, ref scrollPos, scrollRect);
+            Rect rect2 = new Rect(0f, 0f, scrollRect.width, 99999f);
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(rect2);
+            listing.verticalSpacing = listingStandard.verticalSpacing;
+            foreach (SettingContainer setting in settings)
+            {
+                setting.drawSetting(listing, selectedMod);
+            }
+            listing.End();
+            Widgets.EndScrollView();
+        }
+
+        private int calcHeight(List<SettingContainer> settings)
+        {
+            int h = 0;
+            if (settings != null)
+            {
+                foreach (SettingContainer setting in settings)
+                {
+                    h += setting.getHeight();
+                }
+            }
+            return h;
+        }
+
+        public override int getHeight() { return ((int)height); }
+    }
 }
