971cc6dd314b8d2baa7bcaf30552aace74572e1b
15adhami <15adhami@gmail.com>
2021-08-21 16:34:52 -0700

    - Changed C# interface

---
+++ Source/SettingsManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlExtensions
+{
+    public static class SettingsManager
+    {
+        public static string GetSetting(string modId, string key)
+        {
+            return XmlMod.allSettings.dataDict[modId + ";" + key];
+        }
+
+        public static bool TryGetSetting(string modId, string key, out string value)
+        {
+            string temp = "";
+            bool b;
+            b = XmlMod.allSettings.dataDict.TryGetValue(modId + ";" + key, out temp);
+            value = temp;
+            return b;
+        }
+    }
+}
