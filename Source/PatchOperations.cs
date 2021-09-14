fd0e97109ab72e99576afbbd2c980bdaa6a91f43
15adhami <15adhami@gmail.com>
2021-09-13 21:31:08 -0700

    - Fixed bug in PatchDefs where errors wouldn't be reported

--- Source/PatchOperations.cs
+++ Source/PatchOperations.cs
@@ -548,19 +548,33 @@ namespace XmlExtensions
 
         protected override bool ApplyWorker(XmlDocument xml)
         {
-            PatchDef patchDef = DirectXmlToObject.ObjectFromXml<PatchDef>(xml.SelectSingleNode("Defs/XmlExtensions.PatchDef[@Name=\""+patchName+"\"]"), false);
-            XmlContainer newContainer = patchDef.apply;
-            if(arguments != null)
+            try
             {
-                newContainer = Helpers.substituteVariablesXmlContainer(patchDef.apply, patchDef.parameters, arguments, patchDef.brackets);
-            }            
-            int errNum = 0;
-            if (!Helpers.runPatchesInXmlContainer(newContainer, xml, ref errNum))
+                XmlNode node = xml.SelectSingleNode("Defs/XmlExtensions.PatchDef[@Name=\"" + patchName + "\"]");
+                if (node == null)
+                {
+                    PatchManager.errors.Add("XmlExtensions.ApplyPatch(patchName=" + patchName + "): Could not find a patch with the given patchName");
+                    return false;
+                }
+                PatchDef patchDef = DirectXmlToObject.ObjectFromXml<PatchDef>(node, false);
+                XmlContainer newContainer = patchDef.apply;
+                if (arguments != null)
+                {
+                    newContainer = Helpers.substituteVariablesXmlContainer(patchDef.apply, patchDef.parameters, arguments, patchDef.brackets);
+                }
+                int errNum = 0;
+                if (!Helpers.runPatchesInXmlContainer(newContainer, xml, ref errNum))
+                {
+                    PatchManager.errors.Add("XmlExtensions.ApplyPatch(patchName=" + patchName + "): Error in the operation at position=" + errNum.ToString());
+                    return false;
+                }
+                return true;
+            }
+            catch(Exception e)
             {
-                PatchManager.errors.Add("XmlExtensions.ApplyPatch(patchName=" + patchName + "): Error in the operation at position=" + errNum.ToString());
+                PatchManager.errors.Add("XmlExtensions.ApplyPatch(patchName=" + patchName + "): " + e.Message);
                 return false;
             }
-            return true;
         }
     }
 }
