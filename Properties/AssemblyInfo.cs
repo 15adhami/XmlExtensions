c4e9bd31b1a47900b5fffa82306133884543f2ef
15adhami <15adhami@gmail.com>
2021-09-28 18:15:47 -0700

    - Edit metadata

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,4 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.5.4")]
+[assembly: AssemblyInformationalVersion("1.5.5")]
+[assembly: AssemblyFileVersion("1.5.5")]
