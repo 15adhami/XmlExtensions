169c2ed20d8e83a42ad938a4203c388bd8e870b2
15adhami <15adhami@gmail.com>
2021-09-30 03:30:20 -0700

    - Boolean operations support lists

--- Source/PatchOperations/Boolean/And.cs
+++ Source/PatchOperations/Boolean/And.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml;
 
 namespace XmlExtensions.Boolean
@@ -7,26 +8,51 @@ namespace XmlExtensions.Boolean
     {
         protected BooleanBase condition1 = null;
         protected BooleanBase condition2 = null;
+        public List<BooleanBase> conditions;
 
 
         protected override bool evaluation(ref bool b, XmlDocument xml)
         {
             try
             {
-                bool b1 = false;
-                if (!condition1.evaluate(ref b1, xml))
+                if (conditions == null)
                 {
-                    PatchManager.errors.Add(this.GetType().ToString() + ": Failed to evaluate <condition1>");
-                    return false;
+                    bool b1 = false;
+                    if (!condition1.evaluate(ref b1, xml))
+                    {
+                        PatchManager.errors.Add(this.GetType().ToString() + ": Failed to evaluate <condition1>");
+                        return false;
+                    }
+                    bool b2 = false;
+                    if (!condition2.evaluate(ref b2, xml))
+                    {
+                        PatchManager.errors.Add(this.GetType().ToString() + ": Failed to evaluate <condition2>");
+                        return false;
+                    }
+                    b = b1 && b2;
+                    return true;
                 }
-                bool b2 = false;
-                if (!condition2.evaluate(ref b2, xml))
+                else
                 {
-                    PatchManager.errors.Add(this.GetType().ToString() + ": Failed to evaluate <condition2>");
-                    return false;
+                    bool b1 = true;
+                    int num = 0;
+                    foreach(BooleanBase condition in conditions)
+                    {
+                        num++;
+                        if (!condition.evaluate(ref b1, xml))
+                        {
+                            PatchManager.errors.Add(this.GetType().ToString() + ": Failed to evaluate the condition at position=" + num.ToString());
+                            return false;
+                        }
+                        if(!b1)
+                        {
+                            b = b1;
+                            return true;
+                        }
+                    }
+                    b = true;
+                    return true;
                 }
-                b = b1 && b2;
-                return true;
             }
             catch (Exception e)
             {
