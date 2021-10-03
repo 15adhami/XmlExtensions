db9b7c585a1b07e0f4dc937aa1fb2ed01d758274
15adhami <15adhami@gmail.com>
2021-10-03 05:17:10 -0700

    - Added PatchOperationSafeReplace

---
+++ Source/PatchOperations/PatchOperations/PatchOperationSafeReplace.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+
+namespace XmlExtensions
+{
+    public class PatchOperationSafeReplace : PatchOperationExtendedPathed
+	{
+		private XmlContainer value;
+
+		protected override bool applyWorker(XmlDocument xml)
+		{
+            try
+            {
+				XmlNode node = value.node;
+				XmlNodeList nodeList = xml.SelectNodes(xpath);
+				foreach (XmlNode xmlNode in nodeList)
+				{
+					XmlNode parentNode = xmlNode.ParentNode;
+					foreach (XmlNode childNode in node.ChildNodes)
+					{
+						parentNode.InsertBefore(parentNode.OwnerDocument.ImportNode(childNode, deep: true), xmlNode);
+					}
+					parentNode.RemoveChild(xmlNode);
+				}
+				return true;
+			}
+			catch (Exception e)
+			{
+				PatchManager.errors.Add("XmlExtensions.PatchOperationSafeReplace(xpath=" + xpath + "): " + e.Message);
+				return false;
+			}
+		}
+	}
+}
