059fe96e36ac8007cdd1141f40e041f1f32709cd
15adhami <15adhami@gmail.com>
2021-10-09 23:57:25 -0700

    - Modified ScrollArea (can't use scrollwheel)

--- Source/XmlObjects/Setting/Settings/ScrollArea.cs
+++ Source/XmlObjects/Setting/Settings/ScrollArea.cs
@@ -6,8 +6,8 @@ namespace XmlExtensions.Setting
 {
     public class ScrollArea : SettingContainer
     {
-        public float height = 72;
-        public float innerWidth = -1;
+        public float height = 144;
+        public float innerWidth = 1000;
         public List<SettingContainer> settings;
 
         private Vector2 horPos = Vector2.zero;
@@ -15,29 +15,20 @@ namespace XmlExtensions.Setting
 
         protected override bool Init(string selectedMod)
         {
-            if (innerWidth < 0)
-            {
-                innerWidth = 1000;
-            }
             return InitializeSettingsList(selectedMod, settings);
         }
 
         protected override float CalculateHeight(float width, string selectedMod)
         {
-            return height;
+            return height + 16;
         }
 
         protected override void DrawSettingContents(Rect inRect, string selectedMod)
         {
-            float h = GetHeightSettingsList(innerWidth, selectedMod, settings);
-            Rect vertRect = new Rect(0, 0, inRect.width, height);
-            Rect horRect = new Rect(0, 0, inRect.width - 20f, height);
-            Rect scrollRect = new Rect(0, 0, innerWidth, h);
-            
-            Widgets.ScrollHorizontal(horRect, ref horPos, scrollRect);
-            Widgets.BeginScrollView(vertRect, ref vertPos, scrollRect);
-            Rect rect2 = new Rect(0f, 0f, innerWidth, 99999f);
-            DrawSettingsList(rect2, selectedMod, settings);
+            Rect scrollRect = new Rect(0, 0, innerWidth, GetHeightSettingsList(innerWidth, selectedMod, settings));
+            Widgets.BeginScrollView(inRect, ref vertPos, scrollRect);
+            Widgets.ScrollHorizontal(inRect, ref horPos, scrollRect);
+            DrawSettingsList(scrollRect, selectedMod, settings);
             Widgets.EndScrollView();
         }
     }
