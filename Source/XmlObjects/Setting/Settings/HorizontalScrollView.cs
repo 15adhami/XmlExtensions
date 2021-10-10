6d2018e7e2dfb2e9dc87e6b7d4fa32eae442fccd
15adhami <15adhami@gmail.com>
2021-10-09 22:14:42 -0700

    - Added HorizontalScrollView

---
+++ Source/XmlObjects/Setting/Settings/HorizontalScrollView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace XmlExtensions.Setting
+{
+    public class HorizontalScrollView : SettingContainer
+    {
+        public float innerWidth = 1000;
+        public List<SettingContainer> settings;
+
+        private Vector2 horPos = Vector2.zero;
+
+        protected override bool Init(string selectedMod)
+        {
+            return InitializeSettingsList(selectedMod, settings);
+        }
+        
+        protected override float CalculateHeight(float width, string selectedMod)
+        {
+            return GetHeightSettingsList(innerWidth, selectedMod, settings) + 16f;
+        }
+
+        protected override void DrawSettingContents(Rect inRect, string selectedMod)
+        {
+            Rect scrollRect = new Rect(0, 0, innerWidth, inRect.height - 16f);
+            Widgets.BeginScrollView(inRect, ref horPos, scrollRect);
+            Widgets.ScrollHorizontal(inRect, ref horPos, scrollRect);
+            Rect rect2 = new Rect(0f, 0f, innerWidth, 99999f);
+            DrawSettingsList(rect2, selectedMod, settings);
+            Widgets.EndScrollView();
+        }
+    }
+}
