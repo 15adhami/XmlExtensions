4cad1b6dcfb8fa3a3f3d3c7ada8949d02c088896
15adhami <15adhami@gmail.com>
2021-10-11 23:53:52 -0700

    - Added Setting.ApplyActions

---
+++ Source/XmlObjects/Setting/Settings/ApplyActions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using XmlExtensions.Action;
+
+namespace XmlExtensions.Setting
+{
+    public class ApplyActions : SettingContainer
+    {
+        public string label;
+        public string tKey;
+        public string patchDef;
+        public List<MenuAction> actions;
+
+        protected override bool Init(string selectedMod)
+        {
+            if (label == null)
+            {
+                label = "Apply";
+                tKey = "XmlExtensions_Apply";
+            }
+            return true;
+        }
+
+        protected override float CalculateHeight(float width, string selectedMod)
+        {
+            return 30 + GetDefaultSpacing();
+        }
+
+        protected override void DrawSettingContents(Rect inRect, string selectedMod)
+        {
+            if (Widgets.ButtonText(inRect, Helpers.TryTranslate(label, tKey)))
+            {
+                if (actions != null)
+                {
+                    int c = 0;
+                    foreach (MenuAction action in actions)
+                    {
+                        c++;
+                        if (!action.DoAction())
+                        {
+                            Error("Error in the action at position=" + c.ToString());
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
