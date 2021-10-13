3cced84a417f145b7c4f27f42ff5cbdb33c4c1c2
15adhami <15adhami@gmail.com>
2021-10-12 23:17:22 -0700

    - Fixed bug in DefDatabaseOperationInsert

--- Source/PatchOperations/DefDatabase/DefDatabaseOperationInsert.cs
+++ Source/PatchOperations/DefDatabase/DefDatabaseOperationInsert.cs
@@ -24,21 +24,21 @@ namespace XmlExtensions
                 Error("Failed to find an object with the given path");
                 return false;
             }
-            if (obj.GetType().HasGenericDefinition(typeof(List<>)))
+            if (parentObj.GetType().HasGenericDefinition(typeof(List<>)))
             {
                 XmlNodeList nodes = value.node.ChildNodes;
                 if (order == Order.Append)
                 {
                     for (int i = 0; i < nodes.Count; i++)
                     {
-                        AccessTools.Method(obj.GetType(), "Insert").Invoke(obj, new object[] { listIndex + 1, NodeToObject(nodes[i], obj.GetType().GetGenericArguments()[0]) });
+                        AccessTools.Method(parentObj.GetType(), "Insert").Invoke(parentObj, new object[] { listIndex + 1, NodeToObject(nodes[i], parentObj.GetType().GetGenericArguments()[0]) });
                     }
                 }
                 else
                 {
                     for (int i = nodes.Count - 1; i >= 0; i--)
                     {
-                        AccessTools.Method(obj.GetType(), "Insert").Invoke(obj, new object[] { listIndex, NodeToObject(nodes[i], obj.GetType().GetGenericArguments()[0]) });
+                        AccessTools.Method(parentObj.GetType(), "Insert").Invoke(parentObj, new object[] { listIndex, NodeToObject(nodes[i], parentObj.GetType().GetGenericArguments()[0]) });
                     }
                 }
             }
