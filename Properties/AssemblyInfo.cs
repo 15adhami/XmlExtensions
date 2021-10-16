53545494c6cf7e0e7fc2d0a736f48ccba2f138ba
15adhami <15adhami@gmail.com>
2021-10-16 00:30:18 -0700

    - New assembly

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.6.1")]
-[assembly: AssemblyFileVersion("1.6.1")]
+[assembly: AssemblyInformationalVersion("1.6.2")]
+[assembly: AssemblyFileVersion("1.6.2")]
