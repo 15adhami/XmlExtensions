c97e4e593fff96494a8c5af5f3a0a36477731f67
15adhami <15adhami@gmail.com>
2021-10-22 15:17:31 -0700

    - Added DefDatabaseAddDef - Added DefDatabaseSetObject

---
+++ Source/PatchOperations/DefDatabase/DefDatabaseOperationAddDef.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+using System.Xml;
+using Verse;
+
+namespace XmlExtensions
+{
+    public class DefDatabaseOperationAddDef : DefDatabaseOperation
+    {
+        public XmlContainer value;
+
+        protected override bool DoPatch()
+        {
+            foreach (XmlNode node in value.node.ChildNodes)
+            {
+                Type type = GetDefType(node.Name);
+                Type tempType = GenTypes.GetTypeInAnyAssembly(node.Name, "Verse");
+                if (tempType == null)
+                {
+                    tempType = GenTypes.GetTypeInAnyAssembly(node.Name, "RimWorld");
+                    if (tempType == null)
+                    {
+                        return false;
+                    }
+                }
+                MethodInfo genericMethod = typeof(DirectXmlToObject).GetMethod("ObjectFromXml").MakeGenericMethod(tempType);
+                type.GetMethod("Add", new Type[] { tempType }).Invoke(null, new object[] { genericMethod.Invoke(null, new object[] { node, false }) });
+            }
+            return true;
+        }
+    }
+}
---
+++ Source/PatchOperations/DefDatabase/DefDatabaseSetObject.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Xml;
+using Verse;
+
+namespace XmlExtensions
+{
+    public class DefDatabaseSetObject : DefDatabaseOperationPathed
+    {
+        public string defType2;
+        public string defName2;
+        public string objPath2;
+
+        protected override bool DoPatch()
+        {
+            List<string> list = CreateComponents(objPath);
+            AccessTools.Field(parentObj.GetType(), list[list.Count - 1]).SetValue(parentObj, FindObject(GetDef(defType2, defName2), objPath2));
+            return true;
+        }
+    }
+}
