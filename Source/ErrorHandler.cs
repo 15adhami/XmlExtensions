52c324eaa1edf87735f904044e5de58cc1c65a1e
Imran Adham <15adhami@gmail.com>
2021-10-30 15:25:25 -0700

    - Changed access modifiers to private protected

--- Source/ErrorHandler.cs
+++ Source/ErrorHandler.cs
@@ -7,28 +7,14 @@ namespace XmlExtensions
     /// </summary>
     public abstract class ErrorHandler
     {
-        /// <summary>
-        /// The list of values of the relevant fields
-        /// </summary>
-        protected List<string> exceptionVals;
+        private List<string> exceptionVals;
+        private List<string> exceptionFields;
 
-        /// <summary>
-        /// The names of the fields that should be displayed in parenthesis
-        /// </summary>
-        protected List<string> exceptionFields;
-
-        /// <summary>
-        /// Override this method to set the relevaant fields for error handling
-        /// </summary>
-        protected internal virtual void SetException()
+        private protected virtual void SetException()
         {
         }
 
-        /// <summary>
-        /// Helper method to set the exceptions
-        /// </summary>
-        /// <param name="values">Formatted as: fieldVal1, "fieldName1", ...</param>
-        protected internal void CreateExceptions(params string[] values)
+        private protected void CreateExceptions(params string[] values)
         {
             exceptionFields = new List<string>();
             exceptionVals = new List<string>();
@@ -65,13 +51,7 @@ namespace XmlExtensions
             ErrorManager.AddError(str + ": " + msg);
         }
 
-        /// <summary>
-        /// Throw an error message using the given fields
-        /// </summary>
-        /// <param name="vals">An array containing the values of the fields</param>
-        /// <param name="fields">An array containing the names of the fields</param>
-        /// <param name="msg">The error message</param>
-        protected internal void Error(string[] vals, string[] fields, string msg)
+        private protected void Error(string[] vals, string[] fields, string msg)
         {
             string str = GetType().ToString();
             if (vals != null)
@@ -86,20 +66,12 @@ namespace XmlExtensions
             ErrorManager.AddError(str + ": " + msg);
         }
 
-        /// <summary>
-        /// Throw this error if a tag is null
-        /// </summary>
-        /// <param name="node">The name of the node</param>
-        protected internal void NullError(string node)
+        private protected void NullError(string node)
         {
             Error("<" + node + "> is null");
         }
 
-        /// <summary>
-        /// Throw this error if the xpath failed to return a node
-        /// </summary>
-        /// <param name="node">The name of the node containing the xpath</param>
-        protected internal void XPathError(string node = "xpath")
+        private protected void XPathError(string node = "xpath")
         {
             Error("Failed to find a node referenced by <" + node + ">");
         }
--- Source/XmlObjects/Actions/ActionContainer.cs
+++ Source/XmlObjects/Actions/ActionContainer.cs
@@ -2,15 +2,29 @@
 
 namespace XmlExtensions.Action
 {
+    /// <summary>
+    /// Inherit from this class in order to embed C# code into XML
+    /// </summary>
     public abstract class ActionContainer : ErrorHandler
     {
+        /// <summary>
+        /// If the Action is applied in a SettingsMenuDef, then this field will automatically be set to the correct modId
+        /// </summary>
         public string modId;
+
+        /// <summary>
+        /// If you want to return a value to be used by the XML, set this field to that value
+        /// </summary>
         public object output = null;
 
-        protected sealed override void SetException()
+        private protected sealed override void SetException()
         {
         }
 
+        /// <summary>
+        /// Runs the ApplyAction() method of the ActionContainer
+        /// </summary>
+        /// <returns>true if it succeeded, false if there was an error</returns>
         public bool DoAction()
         {
             try
@@ -28,6 +42,10 @@ namespace XmlExtensions.Action
             return true;
         }
 
+        /// <summary>
+        /// The method you should implement
+        /// </summary>
+        /// <returns>Return false if there was an error, true otherwise</returns>
         protected virtual bool ApplyAction()
         {
             return true;
