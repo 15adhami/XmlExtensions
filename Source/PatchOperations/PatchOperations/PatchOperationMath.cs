50428ca0b6513a91b9988ad04e635f473ed1c3f9
15adhami <15adhami@gmail.com>
2022-03-15 23:49:24 -0700

    - Added rangeMin and rangeMax to PatchOperationMath

--- Source/PatchOperations/PatchOperations/PatchOperationMath.cs
+++ Source/PatchOperations/PatchOperations/PatchOperationMath.cs
@@ -7,6 +7,15 @@ namespace XmlExtensions
         protected string value;
         protected bool fromXml = false;
         protected string operation;
+        protected RangeEnd rangeMin = null;
+        protected RangeEnd rangeMax = null;
+
+        protected class RangeEnd
+        {
+            public string value;
+            public string operation;
+            public bool fromXml = false;
+        }
 
         protected override bool PreCheck(XmlDocument xml)
         {
@@ -46,7 +55,62 @@ namespace XmlExtensions
                     valueStored = value;
                 }
                 node2 = xmlNode.Clone();
-                node2.InnerText = Helpers.OperationOnString(xmlNode.InnerText, valueStored, operation);
+                bool isRange = xmlNode.InnerText.Contains("~");
+                if (isRange)
+                {
+                    string[] strSplitted = xmlNode.InnerText.Split('~');
+                    string valueMin = valueStored;
+                    string operationMin = operation;
+                    if (rangeMin != null)
+                    {
+                        if (rangeMin.operation != null)
+                        {
+                            operationMin = rangeMin.operation;
+                        }
+                        if (rangeMin.value != null)
+                        {
+                            valueMin = rangeMin.value;
+                            if (rangeMin.fromXml)
+                            {
+                                XmlNode node = xml.SelectSingleNode(valueMin);
+                                if (node == null)
+                                {
+                                    XPathError("valueMin/value");
+                                    return false;
+                                }
+                                valueMin = node.InnerText;
+                            }
+                        }
+                    }
+                    string valueMax = valueStored;
+                    string operationMax = operation;
+                    if (rangeMax != null)
+                    {
+                        if (rangeMax.operation != null)
+                        {
+                            operationMax = rangeMax.operation;
+                        }
+                        if (rangeMax.value != null)
+                        {
+                            valueMax = rangeMax.value;
+                            if (rangeMax.fromXml)
+                            {
+                                XmlNode node = xml.SelectSingleNode(valueMax);
+                                if (node == null)
+                                {
+                                    XPathError("valueMax/value");
+                                    return false;
+                                }
+                                valueMax = node.InnerText;
+                            }
+                        }
+                    }
+                    node2.InnerText = Helpers.OperationOnString(strSplitted[0], valueMin, operationMin) + "~" + Helpers.OperationOnString(strSplitted[1], valueMax, operationMax);
+                }
+                else
+                {
+                    node2.InnerText = Helpers.OperationOnString(xmlNode.InnerText, valueStored, operation);
+                }
                 parentNode.InsertBefore(parentNode.OwnerDocument.ImportNode(node2, true), xmlNode);
                 parentNode.RemoveChild(xmlNode);
             }
