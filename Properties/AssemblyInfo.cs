ff6fab672f3523dcf9441c05097295ef7ae0fc45
15adhami <15adhami@gmail.com>
2022-03-16 19:37:01 -0700

    - Added rangeMin and rangeMax to PatchOperationMath

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Imranfish")]
 [assembly: AssemblyProduct("XmlExtensions")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyInformationalVersion("1.6.5")]
+[assembly: AssemblyFileVersion("1.6.5")]
