01044c96ec2629ab0a4365a25e757d355edd4d89
15adhami <15adhami@gmail.com>
2022-05-15 03:02:16 -0700

    - Added anchor tag to Setting.Numeric

--- Source/XmlObjects/Setting/Settings/Numeric.cs
+++ Source/XmlObjects/Setting/Settings/Numeric.cs
@@ -8,6 +8,14 @@ namespace XmlExtensions.Setting
         public float min;
         public float max;
         public string tKey;
+        public Anchor anchor = Anchor.Left;
+
+        public enum Anchor
+        {
+            Left = TextAnchor.MiddleLeft,
+            Middle = TextAnchor.MiddleCenter,
+            Right = TextAnchor.MiddleRight
+        }
 
         protected override float CalculateHeight(float width, string selectedMod)
         {
@@ -18,7 +26,19 @@ namespace XmlExtensions.Setting
         {
             float f = float.Parse(SettingsManager.GetSetting(selectedMod, key));
             string buf = f.ToString();
-            Widgets.TextFieldNumericLabeled(inRect, Helpers.TryTranslate(label, tKey), ref f, ref buf, min, max);
+            if (label != null)
+            {
+                Rect rect2 = inRect.LeftHalf().Rounded();
+                Rect rect3 = inRect.RightHalf().Rounded();
+                Verse.Text.Anchor = (TextAnchor)anchor;
+                Widgets.Label(rect2, Helpers.TryTranslate(label, tKey));
+                Verse.Text.Anchor = TextAnchor.UpperLeft;
+                Widgets.TextFieldNumeric(rect3, ref f, ref buf, min, max);
+            }
+            else
+            {
+                Widgets.TextFieldNumeric(inRect, ref f, ref buf, min, max);
+            }
             SettingsManager.SetSetting(selectedMod, key, f.ToString());
         }
     }
