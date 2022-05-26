627d1abca783c3d13a3e9538a55b485abe98b24c
15adhami <15adhami@gmail.com>
2022-05-25 20:39:27 -0700

    - v1.6.6

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.6.5")]
-[assembly: AssemblyFileVersion("1.6.5")]
+[assembly: AssemblyInformationalVersion("1.6.6")]
+[assembly: AssemblyFileVersion("1.6.6")]
