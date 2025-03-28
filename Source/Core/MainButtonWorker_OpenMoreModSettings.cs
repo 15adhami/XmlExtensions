7d440c93907176fae636267fe1183a1d1f443cb8
Jia_RG <jiasust@foxmail.com>
2025-03-28 20:27:20 +0800

    Fix Alignment was MiddleLeft at end of frame error

--- Source/Core/MainButtonWorker_OpenMoreModSettings.cs
+++ Source/Core/MainButtonWorker_OpenMoreModSettings.cs
@@ -1,13 +1,19 @@
 ﻿using RimWorld;
 using Verse;
+using Verse.Sound;
 
 namespace XmlExtensions
 {
     internal class MainButtonWorker_OpenMoreModSettings : MainButtonWorker
     {
+        public MainButtonDef OpenTab => Find.WindowStack.WindowOfType<MainTabWindow>()?.def;
+        
         public override void Activate()
         {
+            if (this.OpenTab != null)
+                Find.WindowStack.TryRemove((Window) this.OpenTab.TabWindow, false);
             Find.WindowStack.Add(new XmlExtensions_MenuModSettings());
+            SoundDefOf.TabOpen.PlayOneShotOnCamera();
         }
     }
 }
