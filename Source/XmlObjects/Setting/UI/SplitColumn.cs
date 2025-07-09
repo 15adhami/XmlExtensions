fb0fde23e69caf5e2bba03583f489b08d2b0ec8c
15adhami <15adhami@gmail.com>
2025-07-08 19:16:22 -0700

    Added <pixels> and <position> argument to SplitColumn

--- Source/XmlObjects/Setting/UI/SplitColumn.cs
+++ Source/XmlObjects/Setting/UI/SplitColumn.cs
@@ -11,8 +11,20 @@ namespace XmlExtensions.Setting
         public float pixels = -1f;
         public List<SettingContainer> leftCol;
         public List<SettingContainer> rightCol;
+        public Position position = Position.Top;
         public bool drawLine = false;
-        private int gapSize = 6;
+        public int gapSize = 6;
+
+        public enum Position
+        {
+            Top,
+            Middle,
+            Bottom
+        }
+
+        private float leftHeight = 0;
+        private float rightHeight = 0;
+        private float totalHeight = 0;
 
         protected override bool Init(string selectedMod)
         {
@@ -32,47 +44,63 @@ namespace XmlExtensions.Setting
             if (pixels >= 0)
             {
                 float leftSize = Math.Min(width - gapSize, pixels);
-                return Math.Max(CalculateHeightSettingsList(leftSize, selectedMod, leftCol), CalculateHeightSettingsList(width - leftSize - gapSize, selectedMod, leftCol));
+                leftHeight = CalculateHeightSettingsList(leftSize, selectedMod, leftCol);
+                rightHeight = CalculateHeightSettingsList(width - leftSize - gapSize, selectedMod, leftCol);
             }
             else
             {
-                return Math.Max(CalculateHeightSettingsList(width * split - gapSize / 2f, selectedMod, leftCol), CalculateHeightSettingsList(width * (1 - split) - gapSize / 2f, selectedMod, rightCol));
+                leftHeight = CalculateHeightSettingsList(width * split - gapSize / 2f, selectedMod, leftCol);
+                rightHeight = CalculateHeightSettingsList(width * (1 - split) - gapSize / 2f, selectedMod, rightCol);
             }
+            totalHeight = Math.Max(leftHeight, rightHeight);
+            return totalHeight;
         }
 
         protected override void DrawSettingContents(Rect inRect, string selectedMod)
         {
+            float leftWidth, rightWidth;
+
             if (pixels >= 0)
             {
-                float leftSize = Math.Min(inRect.width - gapSize, pixels);
-                float rightSize = inRect.width - leftSize - 2 * (int)gapSize;
-                Rect leftRect = inRect.LeftPartPixels(leftSize);
-                Rect rightRect = inRect.RightPartPixels(rightSize);
-                DrawSettingsList(leftRect, selectedMod, leftCol);
-                if (drawLine)
-                {
-                    Color color = GUI.color;
-                    GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
-                    GUI.DrawTexture(new Rect(inRect.center.x, rightRect.yMin, 1f, rightRect.height), BaseContent.WhiteTex);
-                    GUI.color = color;
-                }
-                DrawSettingsList(rightRect, selectedMod, rightCol);
+                leftWidth = Math.Min(inRect.width - gapSize, pixels);
+                rightWidth = inRect.width - leftWidth - 2 * gapSize;
             }
             else
             {
-                Rect leftRect = inRect.LeftPartPixels(inRect.width * split - (gapSize / 2f));
-                Rect rightRect = inRect.RightPartPixels(inRect.width * (1 - split) - (gapSize / 2f));
-                DrawSettingsList(leftRect, selectedMod, leftCol);
-                if (drawLine)
-                {
-                    Color color = GUI.color;
-                    GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
-                    GUI.DrawTexture(new Rect(inRect.center.x, rightRect.yMin, 1f, rightRect.height), BaseContent.WhiteTex);
-                    GUI.color = color;
-                }
-                DrawSettingsList(rightRect, selectedMod, rightCol);
+                leftWidth = inRect.width * split - gapSize / 2f;
+                rightWidth = inRect.width * (1 - split) - gapSize / 2f;
             }
+
+            float leftYOffset = GetYOffset(position, totalHeight, leftHeight);
+            float rightYOffset = GetYOffset(position, totalHeight, rightHeight);
+
+            Rect leftRect = new Rect(inRect.x, inRect.y + leftYOffset, leftWidth, leftHeight);
+            Rect rightRect = new Rect(inRect.x + inRect.width - rightWidth, inRect.y + rightYOffset, rightWidth, rightHeight);
+
+            DrawSettingsList(leftRect, selectedMod, leftCol);
+
+            if (drawLine)
+            {
+                Color color = GUI.color;
+                GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
+                GUI.DrawTexture(new Rect(inRect.center.x, inRect.y, 1f, totalHeight), BaseContent.WhiteTex);
+                GUI.color = color;
+            }
+
+            DrawSettingsList(rightRect, selectedMod, rightCol);
         }
+
+        private float GetYOffset(Position pos, float total, float col)
+        {
+            return pos switch
+            {
+                Position.Top => 0f,
+                Position.Middle => (total - col) / 2f,
+                Position.Bottom => total - col,
+                _ => 0f
+            };
+        }
+
 
         internal override bool PreOpen(string selectedMod)
         {
