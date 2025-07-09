80dd8f9c991c7701ea4ce662d01c3e10220feb1f
15adhami <15adhami@gmail.com>
2025-07-09 13:09:09 -0700

    Added <key> parameter to Setting.SetColor

--- Source/XmlObjects/Setting/UI/SetColor.cs
+++ Source/XmlObjects/Setting/UI/SetColor.cs
@@ -1,13 +1,19 @@
 ﻿using UnityEngine;
+using Verse;
 
 namespace XmlExtensions.Setting
 {
     internal class SetColor : SettingContainer
     {
         protected Color color = Color.white;
+        public string key;
 
         protected override void DrawSettingContents(Rect inRect, string selectedMod)
         {
+            if (key != null)
+            {
+                color = ParseHelper.FromString<Color>(SettingsManager.GetSetting(selectedMod, key));
+            }
             GUI.color = color;
         }
     }
