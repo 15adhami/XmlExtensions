e5daa20a5fb315f1f3d48483f2211c07afcbd5a1
15adhami <15adhami@gmail.com>
2025-07-10 16:07:33 -0700

    Added Aligned anchor to SplitColumn

--- Source/XmlObjects/Setting/UI/SplitColumn.cs
+++ Source/XmlObjects/Setting/UI/SplitColumn.cs
@@ -19,7 +19,8 @@ namespace XmlExtensions.Setting
         {
             Top,
             Middle,
-            Bottom
+            Bottom,
+            Aligned
         }
 
         private float leftHeight = 0;
@@ -42,20 +43,53 @@ namespace XmlExtensions.Setting
 
         protected override float CalculateHeight(float width, string selectedMod)
         {
-            if (this.width >= 0)
+            if (anchor == Anchor.Aligned)
             {
-                float leftSize = Math.Min(width, this.width - gapSize / 2f);
-                leftHeight = CalculateHeightSettingsList(leftSize, selectedMod, leftCol);
-                rightHeight = CalculateHeightSettingsList(Math.Max(width - leftSize - gapSize, 0), selectedMod, rightCol);
+                float leftWidth, rightWidth;
+
+                if (this.width >= 0)
+                {
+                    leftWidth = Math.Min(width, this.width - gapSize / 2f);
+                    rightWidth = Math.Max(width - leftWidth - gapSize, 0);
+                }
+                else
+                {
+                    leftWidth = width * split - gapSize / 2f;
+                    rightWidth = width * (1 - split) - gapSize / 2f;
+                }
+
+                int count = Math.Max(leftCol?.Count ?? 0, rightCol?.Count ?? 0);
+                float height = 0f;
+
+                for (int i = 0; i < count; i++)
+                {
+                    float lh = (i < (leftCol?.Count ?? 0)) ? leftCol[i].GetHeight(leftWidth, selectedMod) : 0f;
+                    float rh = (i < (rightCol?.Count ?? 0)) ? rightCol[i].GetHeight(rightWidth, selectedMod) : 0f;
+                    height += Math.Max(lh, rh);
+                }
+
+                leftHeight = rightHeight = totalHeight = height;
+                return totalHeight;
             }
             else
             {
-                leftHeight = CalculateHeightSettingsList(width * split - gapSize / 2f, selectedMod, leftCol);
-                rightHeight = CalculateHeightSettingsList(width * (1 - split) - gapSize / 2f, selectedMod, rightCol);
+                if (this.width >= 0)
+                {
+                    float leftSize = Math.Min(width, this.width - gapSize / 2f);
+                    leftHeight = CalculateHeightSettingsList(leftSize, selectedMod, leftCol);
+                    rightHeight = CalculateHeightSettingsList(Math.Max(width - leftSize - gapSize, 0), selectedMod, rightCol);
+                }
+                else
+                {
+                    leftHeight = CalculateHeightSettingsList(width * split - gapSize / 2f, selectedMod, leftCol);
+                    rightHeight = CalculateHeightSettingsList(width * (1 - split) - gapSize / 2f, selectedMod, rightCol);
+                }
+
+                totalHeight = Math.Max(leftHeight, rightHeight);
+                return totalHeight;
             }
-            totalHeight = Math.Max(leftHeight, rightHeight);
-            return totalHeight;
         }
+
 
         protected override void DrawSettingContents(Rect inRect, string selectedMod)
         {
@@ -72,23 +106,65 @@ namespace XmlExtensions.Setting
                 rightWidth = inRect.width * (1 - split) - gapSize / 2f;
             }
 
-            float leftYOffset = GetYOffset(anchor, totalHeight, leftHeight);
-            float rightYOffset = GetYOffset(anchor, totalHeight, rightHeight);
-
-            Rect leftRect = new Rect(inRect.x, inRect.y + leftYOffset, leftWidth, leftHeight);
-            Rect rightRect = new Rect(inRect.x + inRect.width - rightWidth, inRect.y + rightYOffset, rightWidth, rightHeight);
-
-            DrawSettingsList(leftRect, selectedMod, leftCol);
-
-            if (drawLine)
+            if (anchor == Anchor.Aligned)
             {
-                Color color = GUI.color;
-                GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
-                GUI.DrawTexture(new Rect(inRect.center.x, inRect.y, 1f, totalHeight), BaseContent.WhiteTex);
-                GUI.color = color;
-            }
+                float yOffset = inRect.y;
+                int count = Math.Max(leftCol?.Count ?? 0, rightCol?.Count ?? 0);
 
-            DrawSettingsList(rightRect, selectedMod, rightCol);
+                for (int i = 0; i < count; i++)
+                {
+                    SettingContainer left = (i < (leftCol?.Count ?? 0)) ? leftCol[i] : null;
+                    SettingContainer right = (i < (rightCol?.Count ?? 0)) ? rightCol[i] : null;
+
+                    float lh = left?.GetHeight(leftWidth, selectedMod) ?? 0f;
+                    float rh = right?.GetHeight(rightWidth, selectedMod) ?? 0f;
+                    float pairHeight = Math.Max(lh, rh);
+
+                    if (left != null)
+                    {
+                        float yOff = (pairHeight - lh) / 2f;
+                        Rect rect = new Rect(inRect.x, yOffset + yOff, leftWidth, lh);
+                        left.DrawSetting(rect, selectedMod);
+                    }
+
+                    if (right != null)
+                    {
+                        float yOff = (pairHeight - rh) / 2f;
+                        Rect rect = new Rect(inRect.x + inRect.width - rightWidth, yOffset + yOff, rightWidth, rh);
+                        right.DrawSetting(rect, selectedMod);
+                    }
+
+                    yOffset += pairHeight;
+                }
+
+                if (drawLine)
+                {
+                    Color color = GUI.color;
+                    GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
+                    GUI.DrawTexture(new Rect(inRect.center.x, inRect.y, 1f, yOffset - inRect.y), BaseContent.WhiteTex);
+                    GUI.color = color;
+                }
+            }
+            else
+            {
+                float leftYOffset = GetYOffset(anchor, totalHeight, leftHeight);
+                float rightYOffset = GetYOffset(anchor, totalHeight, rightHeight);
+
+                Rect leftRect = new Rect(inRect.x, inRect.y + leftYOffset, leftWidth, leftHeight);
+                Rect rightRect = new Rect(inRect.x + inRect.width - rightWidth, inRect.y + rightYOffset, rightWidth, rightHeight);
+
+                DrawSettingsList(leftRect, selectedMod, leftCol);
+
+                if (drawLine)
+                {
+                    Color color = GUI.color;
+                    GUI.color = color * new Color(1f, 1f, 1f, 0.4f);
+                    GUI.DrawTexture(new Rect(inRect.center.x, inRect.y, 1f, totalHeight), BaseContent.WhiteTex);
+                    GUI.color = color;
+                }
+
+                DrawSettingsList(rightRect, selectedMod, rightCol);
+            }
         }
 
         private float GetYOffset(Anchor pos, float total, float col)
