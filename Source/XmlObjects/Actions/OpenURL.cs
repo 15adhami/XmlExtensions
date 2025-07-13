5e8c78e43823e020a617aee251a2b526bcc47092
15adhami <15adhami@gmail.com>
2025-07-13 02:17:28 -0700

    Added Action.OpenURL

---
+++ Source/XmlObjects/Actions/OpenURL.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace XmlExtensions.Action
+{
+    internal class OpenURL : ActionContainer
+    {
+        public string url;
+
+        protected override bool ApplyAction()
+        {
+            if (url != null) { Application.OpenURL(url); }
+            return true;
+        }
+    }
+}
