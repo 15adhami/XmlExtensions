8bc0dd2751e8e145c76a0c4e6e47bd6c17ee571b
15adhami <15adhami@gmail.com>
2025-07-13 23:21:52 -0700

    Added Action.Log

---
+++ Source/XmlObjects/Actions/Log.cs
@@ -0,0 +1,25 @@
+﻿namespace XmlExtensions.Action
+{
+    internal class Log : ActionContainer
+    {
+        protected string text;
+        protected string warning;
+        protected string error;
+        protected string key;
+
+        protected override bool ApplyAction()
+        {
+            if (text == null && key == null && error == null && warning == null)
+                Verse.Log.Message("XmlExtensions.Log");
+            if (text != null)
+                Verse.Log.Message(text);
+            if (warning != null)
+                Verse.Log.Warning(warning);
+            if (error != null)
+                Verse.Log.Error(error);
+            if (key != null)
+                Verse.Log.Message(SettingsManager.GetSetting(modId, key));
+            return true;
+        }
+    }
+}
