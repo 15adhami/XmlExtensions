d7692bd74e058746e169efd22b796fe69b5a6805
15adhami <15adhami@gmail.com>
2025-07-15 00:47:48 -0700

    Added PatchOperationEditString

---
+++ Source/PatchOperations/PatchOperations/PatchOperationEditString.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml;
+using Verse;
+
+namespace XmlExtensions
+{
+    internal class PatchOperationEditString : PatchOperationExtendedPathed
+    {
+        public string substring;
+        public string replaceWith;
+
+        protected override bool Patch(XmlDocument xml)
+        {
+            substring ??= nodes[0].InnerText;
+            foreach (XmlNode xmlNode in nodes)
+            {
+                if (xmlNode.NodeType == XmlNodeType.Element && xmlNode.InnerText.Contains(substring))
+                {
+                    xmlNode.InnerText = xmlNode.InnerText.Replace(substring, replaceWith);
+                }
+            }
+            return true;
+        }
+    }
+}
