d05ed76d28e0ba16db2d68d228a64e773171d97c
15adhami <15adhami@gmail.com>
2025-07-20 20:29:42 -0700

    Added modes to ForEachLoop, changed default variable to {value}

--- Source/PatchOperations/Control/ForEachLoop.cs
+++ Source/PatchOperations/Control/ForEachLoop.cs
@@ -8,9 +8,18 @@ namespace XmlExtensions
     {
         public string xpath;
         protected XmlContainer apply;
-        protected string storeIn = "path";
+        protected string storeIn = "value";
         protected string brackets = "{}";
         protected List<string> values;
+        protected Mode mode = Mode.XPath;
+
+        public enum Mode
+        {
+            XPath,
+            InnerText,
+            Node,
+            LocalPatch
+        }
 
         protected override void SetException()
         {
@@ -22,7 +31,19 @@ namespace XmlExtensions
 
         protected override bool Patch(XmlDocument xml)
         {
-            if (values == null)
+            if (values != null)
+            {
+                foreach (string value in values)
+                {
+                    XmlContainer newContainer = Helpers.SubstituteVariableXmlContainer(apply, storeIn, value, brackets);
+                    if (!RunPatches(newContainer, xml, false))
+                    {
+                        Error("Error with value=" + value);
+                        return false;
+                    }
+                }
+            }
+            else if (mode == Mode.XPath)
             {
                 XmlNodeList nodes = xml.SelectNodes(xpath);
                 if (nodes.Count == 0)
@@ -37,15 +58,70 @@ namespace XmlExtensions
                     if (!RunPatches(newContainer, xml)) { return false; }
                 }
             }
-            else
+            else if (mode == Mode.InnerText)
             {
-                foreach (string value in values)
+                XmlNodeList nodes = xml.SelectNodes(xpath);
+                if (nodes.Count == 0)
                 {
-                    XmlContainer newContainer = Helpers.SubstituteVariableXmlContainer(apply, storeIn, value, brackets);
-                    if (!RunPatches(newContainer, xml, false))
+                    XPathError();
+                    return false;
+                }
+                foreach (XmlNode xmlNode in nodes)
+                {
+                    string innerText = xmlNode.InnerText;
+                    XmlContainer newContainer = Helpers.SubstituteVariableXmlContainer(apply, storeIn, innerText, brackets);
+                    if (!RunPatches(newContainer, xml)) { return false; }
+                }
+            }
+            else if (mode == Mode.Node)
+            {
+                XmlNodeList nodes = xml.SelectNodes(xpath);
+                if (nodes.Count == 0)
+                {
+                    XPathError();
+                    return false;
+                }
+                foreach (XmlNode xmlNode in nodes)
+                {
+                    string outerXml = xmlNode.OuterXml;
+                    XmlContainer newContainer = Helpers.SubstituteVariableXmlContainer(apply, storeIn, outerXml, brackets);
+                    if (!RunPatches(newContainer, xml)) { return false; }
+                }
+            }
+            else if (mode == Mode.LocalPatch)
+            {
+                XmlNodeList nodes = xml.SelectNodes(xpath);
+                if (nodes.Count == 0)
+                {
+                    XPathError();
+                    return false;
+                }
+
+                foreach (XmlNode xmlNode in nodes)
+                {
+                    XmlDocument tempDoc = new();
+                    XmlNode importedNode = tempDoc.ImportNode(xmlNode, true);
+                    tempDoc.AppendChild(importedNode);
+
+                    XmlContainer newContainer = Helpers.SubstituteVariableXmlContainer(apply, storeIn, xmlNode.InnerText, brackets);
+                    if (!RunPatches(newContainer, tempDoc))
                     {
-                        Error("Error with value=" + value);
+                        Error("Patch failed on local node:\n" + xmlNode.OuterXml);
                         return false;
+                    }
+
+                    xmlNode.RemoveAll();
+                    foreach (XmlAttribute attr in importedNode.Attributes)
+                    {
+                        XmlAttribute newAttr = xml.OwnerDocument.CreateAttribute(attr.Name);
+                        newAttr.Value = attr.Value;
+                        ((XmlElement)xmlNode).SetAttributeNode(newAttr);
+                    }
+
+                    foreach (XmlNode child in importedNode.ChildNodes)
+                    {
+                        XmlNode newChild = xml.ImportNode(child, true);
+                        xmlNode.AppendChild(newChild);
                     }
                 }
             }
