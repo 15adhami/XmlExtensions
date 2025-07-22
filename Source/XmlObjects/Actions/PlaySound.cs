87095a5a62d08cdbe90416528e0532c7f04f2a80
15adhami <15adhami@gmail.com>
2025-07-21 18:26:01 -0700

    Added PlaySound

---
+++ Source/XmlObjects/Actions/PlaySound.cs
@@ -0,0 +1,42 @@
+﻿using Verse;
+using Verse.Sound;
+
+namespace XmlExtensions.Action
+{
+    internal class PlaySound : ActionContainer
+    {
+        public string soundDefName;
+
+        private SoundDef cachedSound;
+
+        protected override bool Init()
+        {
+            if (string.IsNullOrEmpty(soundDefName))
+            {
+                Error("soundDefName is null or empty");
+                return false;
+            }
+
+            cachedSound = DefDatabase<SoundDef>.GetNamedSilentFail(soundDefName);
+            if (cachedSound == null)
+            {
+                Error($"SoundDef \"{soundDefName}\" not found.");
+                return false;
+            }
+
+            return true;
+        }
+
+        protected override bool ApplyAction()
+        {
+            if (cachedSound == null)
+            {
+                Error("Cached SoundDef is null (this should not happen if Init succeeded)");
+                return false;
+            }
+
+            cachedSound.PlayOneShotOnCamera();
+            return true;
+        }
+    }
+}
