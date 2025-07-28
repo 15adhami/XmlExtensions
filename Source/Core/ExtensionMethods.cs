174fcd43bb3ea85cf4edfd075f0eef6befccfc8c
15adhami <15adhami@gmail.com>
2025-07-28 01:28:05 -0700

    Added extension method for Rect.MiddlePartPixels

---
+++ Source/Core/ExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace XmlExtensions
+{
+    internal static class ExtensionMethods
+    {
+        extension(Rect rect)
+        {
+            internal Rect MiddlePartPixels(float width, float height)
+            {
+                return rect.LeftPartPixels(width + 0.5f * (rect.width - width)).RightPartPixels(width);
+            }
+        }
+    }
+}
