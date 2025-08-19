4b0a65d15659e34be7beff86687eda8081239cf4
15adhami <15adhami@gmail.com>
2025-08-19 04:36:57 -0700

    Updated version number

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.9.1")]
-[assembly: AssemblyFileVersion("1.9.1")]
+[assembly: AssemblyInformationalVersion("1.9.2")]
+[assembly: AssemblyFileVersion("1.9.2")]
