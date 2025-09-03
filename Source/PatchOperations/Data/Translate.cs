497be6636e7dfd4cc309ab94a62612ce0f36e198
15adhami <15adhami@gmail.com>
2025-09-02 19:23:53 -0700

    Added Translate patch operation

---
+++ Source/PatchOperations/Data/Translate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+using Verse;
+
+namespace XmlExtensions
+{
+    internal class Translate : PatchOperationValue
+    {
+        public string tKey;
+
+        protected override void SetException()
+        {
+            CreateExceptions(storeIn, "storeIn", tKey, "tKey");
+        }
+
+        public override bool getValues(List<string> vals, XmlDocument xml)
+        {
+            try
+            {
+                vals.Add(tKey.TranslateSimple());
+            }
+            catch
+            {
+                Error("Given tKey not found");
+                return false;
+            }
+            return true;
+        }
+    }
+}
